96af7ef3e8866081b84a185354ca23391192b027
7ime <lopatsin1990@gmail.com>
2021-04-06 10:45:51 +0300

    reduceNumberByDivisionToMaxSize

--- src/helpers/avatar/get-avatar-bg-color/index.ts
+++ src/helpers/avatar/get-avatar-bg-color/index.ts
@@ -1,11 +1,11 @@
 import {AVATAR_COLORS} from '@constants/avatar'
-import {reduceNumberToMaxSize} from '@helpers/reduce-number-to-max-size'
+import {reduceNumberByDivisionToMaxSize} from '@helpers/reduce-number-by-division-to-max-size'
 
 export const getAvatarBgColor = (initial: string): string => {
     const charCode = initial.charCodeAt(0)
     const bgColors = Array.from(AVATAR_COLORS.values())
 
-    const index = Math.floor(reduceNumberToMaxSize(charCode, bgColors.length - 1))
+    const index = Math.floor(reduceNumberByDivisionToMaxSize(charCode, bgColors.length - 1))
 
     return bgColors[index]
 }
