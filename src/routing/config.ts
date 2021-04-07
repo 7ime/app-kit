9595eb9d6fdc6036df0c78abcf35f37cf32b3430
7ime <lopatsin1990@gmail.com>
2021-04-07 09:43:51 +0300

    routes base url

--- src/routing/config.ts
+++ src/routing/config.ts
@@ -1,8 +1,5 @@
-import {environment} from '@toolbox/environment'
-import {SAMPLE} from '@toolbox/sample'
-
 export const configurationRoutes = Object.freeze(
     {
-        baseUrl: environment.production ? SAMPLE.APP_WEBSITE_BASE_URL : '/'
+        baseUrl: '/'
     }
 )
