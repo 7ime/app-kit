521eaa3f19ad2942292a21db88bbad5f5bd84dbc
7ime <lopatsin1990@gmail.com>
2021-04-07 09:13:10 +0300

    webpack html plugin add public path

--- configs/webpack/tools/plugins.js
+++ configs/webpack/tools/plugins.js
@@ -59,8 +59,9 @@ module.exports = {
             inject: true,
             filename: 'index.html',
             template: resolvePath(`${paths.source}/index.html`),
+            publicPath: isDevMode ? '' : process.env.APP_WEBSITE_BASE_URL,
             chunks: 'bundle',
-            base: '/app'
+            base: '/'
         })
     },
     terserPlugin() {
